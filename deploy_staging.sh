764886979a902a9e46bfda8f22d732c6aac62c75
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-09-25 21:55:54 +0500

    environment variable for staging

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,7 +1,7 @@
 # This script is to restart the server in production mode.
 # This will install all the required libraries.
-git pull origin master
+git pull origin staging
 npm install
 npm install --only=dev
-npm run start:prod
-npm run stop:prod
+npm run start:staging
+npm run stop:staging
