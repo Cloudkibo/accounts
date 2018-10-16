35d793702114f0049adf4e2c62f9e17f9db85957
Sojharo <sojharo@live.com>
2018-10-16 16:45:46 +0500

    Update index.js

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -10,7 +10,7 @@ router.post('/', function (req, res, next) {
   if (req.body.email) {
     passport.authenticate('email-local', function (err, user, info) {
       var error = err || info
-      if (error) return res.status(501).json({status: 'failed', description: 'Internal Server Error', error: '' + JSON.stringify(error)})
+      if (error) return res.status(501).json({status: 'failed', description: error.message, error: '' + JSON.stringify(error)})
       if (!user) return res.json(404).json({message: 'User Not Found'})
       req.user = user
       return auth.setTokenCookie(req, res)
