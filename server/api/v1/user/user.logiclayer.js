440efac1f2c89f29418c2b37aa117afb29f79133
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-10-16 14:16:12 +0500

    log

--- server/api/v1/user/user.logiclayer.js
+++ server/api/v1/user/user.logiclayer.js
@@ -173,9 +173,9 @@ const setEmailBody = (email, tokenString, body) => {
     '<!-- END: Header Container --> </td> </tr> </table> <!-- END: Header --> <!-- BEGIN: Content --> <table class="container content" align="center"> <tr> <td> <table class="row note"> ' +
     '<tr> <td class="wrapper last"> <p> Hello,' + body.name + '<br> Thank you for joining KiboPush. <br>Use the following link to verify your account <br>  </p> <p>To accept invitation please click the following URL to activate your account:</p> <!-- BEGIN: Note Panel --> <table class="twelve columns" style="margin-bottom: 10px"> ' +
     '<tr> <td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;"> <a href="' +
-    config.domain + '/api/email_verification/verify/' +
+    config.domain + '/api/v1/verificationtoken/verify/' +
     tokenString +
-    '"> ' + config.domain + '/api/email_verification/verify/' +
+    '"> ' + config.domain + '/api/v1/verificationtoken/verify/' +
     tokenString +
     '</a> </td> <td class="expander"> </td> </tr> </table> <p> If clicking the URL above does not work, copy and paste the URL into a browser window. </p> <!-- END: Note Panel --> </td> </tr> </table><span class="devider" style="border-bottom: 1px solid #eee;margin: 15px -15px;display: block;"></span> <!-- END: Disscount Content --> </td> </tr> </table> </td> </tr> </table> <!-- END: Content --> <!-- BEGIN: Footer --> <table class="page-footer" align="center" style="width: 100%;background: #2f2f2f;"> <tr> <td class="center" align="center" style="vertical-align: middle;color: #fff;"> <table class="container" align="center"> <tr> <td style="vertical-align: middle;color: #fff;"> <!-- BEGIN: Unsubscribet --> <table class="row"> <tr> <td class="wrapper last" style="vertical-align: middle;color: #fff;"><span style="font-size:12px;"><i>This ia a system generated email and reply is not required.</i></span> </td> </tr> </table> <!-- END: Unsubscribe --> ' +
     '<!-- END: Footer Panel List --> </td> </tr> </table> </td> </tr> </table> <!-- END: Footer --> </td> </tr></table></body>')
