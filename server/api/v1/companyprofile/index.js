f06e53586c6628f9f51e00067d8250a944ba2413
anishachhatwani <i130315@nu.edu.pk>
2018-10-22 13:52:43 +0500

    demo app work

--- server/api/v1/companyprofile/index.js
+++ server/api/v1/companyprofile/index.js
@@ -16,45 +16,60 @@ var stripeWebhook = new StripeWebhook({
 })
 
 router.get('/',
+  auth.isAuthenticated(),
   controller.index)
 
 router.post('/invite',
   validate({body: validationSchema.invitePayload}),
+  auth.isAuthenticated(),
   controller.invite)
 
 router.post('/updateRole',
   validate({body: validationSchema.updateRole}),
+  auth.isAuthenticated(),
   controller.updateRole)
 
 router.post('/removeMember',
   validate({body: validationSchema.removeMember}),
+  auth.isAuthenticated(),
   controller.removeMember)
 
 router.get('/members',
+  auth.isAuthenticated(),
   auth.isAuthenticated(),
   controller.members)
 
 router.post('/updateAutomatedOptions',
   validate({body: validationSchema.updateAutomatedOptions}),
+  auth.isAuthenticated(),
   controller.updateAutomatedOptions)
 
-router.get('/addPlanID', controller.addPlanID)
+router.get('/addPlanID',
+  auth.isAuthenticated(),
+  controller.addPlanID)
+
 router.post('/setCard',
   validate({body: validationSchema.setCard}),
+  auth.isAuthenticated(),
   controller.setCard)
 
 router.post('/updatePlan',
   validate({body: validationSchema.updatePlan}),
+  auth.isAuthenticated(),
   controller.updatePlan)
 
 router.get('/getAutomatedOptions',
+  auth.isAuthenticated(),
   controller.getAutomatedOptions)
 
-router.get('/getKeys', controller.getKeys)
+router.get('/getKeys',
+  auth.isAuthenticated(),
+  controller.getKeys)
 
 // use this url to receive stripe webhook events
 router.post('/stripe/events',
   stripeWebhook.middleware,
+  auth.isAuthenticated(),
   stripeEvents
 )
 
