3e89452316e4699f96d36e3f375ba85b46cfd1aa
Sojharo <sojharo@live.com>
2018-12-10 17:24:39 +0500

    Mime Type

--- server/auth/index.js
+++ server/auth/index.js
@@ -25,6 +25,7 @@ router.get('/scripts/jsonp', (req, res) => {
 
   let token = req.cookies.token
   let callbackOfClient = req.query.callback
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.writeHead(200, {'Content-Type': 'application/javascript'});
   res.send(`${callbackOfClient}("${token}")`)
 })
