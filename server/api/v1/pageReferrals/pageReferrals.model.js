77dc953a27fc3440f7d710c1e0971cef40f6e0ad
anishachhatwani <i130315@nu.edu.pk>
2019-01-04 11:27:19 +0500

    messenger ref url

--- server/api/v1/pageReferrals/pageReferrals.model.js
+++ server/api/v1/pageReferrals/pageReferrals.model.js
@@ -3,7 +3,7 @@ let Schema = mongoose.Schema
 
 const pageReferralsSchema = new Schema({
   companyId: { type: Schema.ObjectId, ref: 'companyprofile' },
-  pageId: {type: String},
+  pageId: { type: Schema.ObjectId, ref: 'pages' },
   ref_parameter: {type: String},
   reply: {type: Schema.Types.Mixed},
   sequenceId: {type: String}
