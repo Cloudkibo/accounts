23fc1066fab7de762aa1492837098cb85f2174e2
sania <sania@cloudkibo.com>
2019-01-11 16:36:51 +0500

    Click to messenger Ads

--- server/api/v1/jsonAd/jsonAdMessages.model.js
+++ server/api/v1/jsonAd/jsonAdMessages.model.js
@@ -4,7 +4,7 @@ let Schema = mongoose.Schema
 let jsonAdMessages = new Schema({
   jsonAdId: {type: Schema.ObjectId, ref: 'jsonAd'},
   jsonAdMessageParentId: {type: String},
-  messageContent: Schema.Types.Mixed
+  messageContent: {type: Schema.Types.Mixed}
 })
 
 module.exports = mongoose.model('jsonAdMessages', jsonAdMessages)
