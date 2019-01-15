348c3dd58d73052e66f29f259227698274142999
sania <sania@cloudkibo.com>
2019-01-15 11:06:23 +0500

    kiboengage #192

--- server/api/v1/pages/index.js
+++ server/api/v1/pages/index.js
@@ -58,9 +58,4 @@ router.post('/deleteWhitelistDomain',
   validate({body: validationSchema.deleteWhitelistDomain}),
   controller.deleteWhitelistDomain)
 
-router.post('/isWhitelisted',
-  auth.isAuthenticated(),
-  validate({body: validationSchema.isWhitelisted}),
-  controller.isWhitelisted)
-
 module.exports = router
