f1b00a539967a9ce3ba6561618c03f66cde3d64c
Imran Shoukat <ishoukat@localhost.localdomain>
2019-01-22 13:45:31 +0500

    put random date script

--- server/api/scripts/controller.js
+++ server/api/scripts/controller.js
@@ -1,6 +1,7 @@
 const logger = require('../../components/logger')
 const TAG = '/api/scripts/controller.js'
 const SusbscribersDataLayer = require('../v1/subscribers/subscribers.datalayer')
+const SubscribersModel = require('../v1/subscribers/Subscribers.model')
 const { callApi } = require('./apiCaller')
 
 exports.normalizeSubscribersDatetime = function (req, res) {
@@ -32,4 +33,61 @@ exports.normalizeSubscribersDatetime = function (req, res) {
     .catch(err => {
       return res.status(500).json({status: 'failed', payload: `Failed to fetch subscribers ${err}`})
     })
+}
+
+function randomDate (start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+}
+
+exports.normalizeSubscribersDatetimeNull = function (req, res) {
+  logger.serverLog(TAG, 'Hit the scripts normalizeDatetime')
+  SubscribersModel.aggregate([
+    {$match: {datetime: null}},
+    {$group: {_id: '$pageId', count: {$sum: 1}}}
+  ]).exec()
+    .then(distintPages => {
+      if (distintPages.length > 0) {
+        distintPages.forEach(page => {
+          SubscribersModel.find({pageId: page._id, datetime: null}).exec()
+            .then(subscribers => {
+              if (subscribers.length > 0) {
+                SubscribersModel.findOne({_id: {$lt: subscribers[0]._id}}).sort({_id: -1}).exec()
+                  .then(startSub => {
+                    if (startSub) {
+                      const startDate = startSub.datetime
+                      SubscribersModel.findOne({_id: {$gt: subscribers[subscribers.length - 1]._id}}).sort({_id: 1}).exec()
+                        .then(endSub => {
+                          if (endSub) {
+                            const endDate = endSub.datetime
+                            subscribers.forEach(sub => {
+                              let rDate = randomDate(new Date(startDate, endDate))
+                              SubscribersModel.update({_id: sub._id}, {datetime: new Date(rDate)}).exec()
+                                .then(updated => {})
+                                .catch(err => {
+                                  return res.status(500).json({status: 'failed', payload: `Failed to update subscriber ${err}`})
+                                })
+                            })
+                          }
+                        })
+                        .catch(err => {
+                          return res.status(500).json({status: 'failed', payload: `Failed to fetch end subscriber ${err}`})
+                        })
+                    }
+                  })
+                  .catch(err => {
+                    return res.status(500).json({status: 'failed', payload: `Failed to fetch start subscriber ${err}`})
+                  })
+              }
+            })
+            .catch(err => {
+              return res.status(500).json({status: 'failed', payload: `Failed to fetch subscribers ${err}`})
+            })
+        })
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({status: 'failed', payload: `Failed to fetch distint pages ${err}`})
+    })
+
+  return res.status(200).json({ status: 'success', payload: 'Normalized successfully!' })
 }
--- server/api/scripts/index.js
+++ server/api/scripts/index.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require('./controller.js')
 
 router.get('/susbscribers/normalizeDatetime', controller.normalizeSubscribersDatetime)
+router.get('/susbscribers/normalizeDatetimeNull', controller.normalizeSubscribersDatetimeNull)
 
 module.exports = router
