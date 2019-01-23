a1066037b0b2af74a3fa0bc95c23e758fcbf216c
Faizan Rasheed <faizanrasheed4@gmail.com>
2019-01-23 14:27:01 +0500

    Modify Server side Logic and remove unnecessary code #4964

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -13,71 +13,71 @@ let TAG = '/server/auth/local/index.js'
 var router = express.Router()
 
 router.post('/', function (req, res, next) {
-  if (req.body.domain) {
-    User.findOne({
-      email: req.body.email.toLowerCase()
-    }, (err, user) => {
-      if (err) {
-        return res.status(501)
-          .json({status: 'failed', description: 'Internal Server Error'})
-      }
-      if (!user) {
-        return res.status(401)
-          .json({
-            status: 'failed',
-            description: 'No account found with this email address.'
-          })
-      }
-      user = user.toObject()
-      if (user.domain !== req.body.domain.toLowerCase()) {
-        return res.status(401)
-          .json({
-            status: 'failed',
-            description: 'This workspace name is not registered with us or your account does not belong to this domain'
-          })
-      }
-      if (user.domain === req.body.domain.toLowerCase() && user.email !== req.body.email.toLowerCase()) {
-        return res.status(401)
-          .json({
-            status: 'failed',
-            description: 'No account found with this email address.'
-          })
-      }
-      logger.serverLog(TAG, `User in login: ${JSON.stringify(user)}`)
-      CompanyUsers.findOne({domain_email: user.domain_email}, (err, companyuser) => {
-        if (err) {
-          return res.status(501)
-            .json({status: 'failed', description: 'Internal Server Error'})
-        }
+  // if (req.body.domain) {
+  //   User.findOne({
+  //     email: req.body.email.toLowerCase()
+  //   }, (err, user) => {
+  //     if (err) {
+  //       return res.status(501)
+  //         .json({status: 'failed', description: 'Internal Server Error'})
+  //     }
+  //     if (!user) {
+  //       return res.status(401)
+  //         .json({
+  //           status: 'failed',
+  //           description: 'No account found with this email address.'
+  //         })
+  //     }
+  //     user = user.toObject()
+  //     if (user.domain !== req.body.domain.toLowerCase()) {
+  //       return res.status(401)
+  //         .json({
+  //           status: 'failed',
+  //           description: 'This workspace name is not registered with us or your account does not belong to this domain'
+  //         })
+  //     }
+  //     if (user.domain === req.body.domain.toLowerCase() && user.email !== req.body.email.toLowerCase()) {
+  //       return res.status(401)
+  //         .json({
+  //           status: 'failed',
+  //           description: 'No account found with this email address.'
+  //         })
+  //     }
+  //     logger.serverLog(TAG, `User in login: ${JSON.stringify(user)}`)
+  //     CompanyUsers.findOne({domain_email: user.domain_email}, (err, companyuser) => {
+  //       if (err) {
+  //         return res.status(501)
+  //           .json({status: 'failed', description: 'Internal Server Error'})
+  //       }
 
-        CompanyProfile.findOne({_id: companyuser.companyId}).populate('planId').exec((err, company) => {
-          if (err) {
-            return res.status(501)
-              .json({status: 'failed', description: 'Internal Server Error'})
-          }
-          if (['plan_C', 'plan_D'].indexOf(company.planId.unique_ID) < 0) {
-            return res.status(401)
-              .json({
-                status: 'failed',
-                description: 'Given account information does not match any team account in our records'
-              })
-          }
-          passport.authenticate('email-local', function (err, user, info) {
-            var error = err || info
-            if (error) return res.status(501).json({status: 'failed', description: error.message, error: '' + JSON.stringify(error)})
-            if (!user) return res.json(404).json({message: 'User Not Found'})
-            req.user = user
-            if (user.facebookInfo) {
-              auth.fetchPages(`https://graph.facebook.com/v2.10/${
-                user.facebookInfo.fbId}/accounts?access_token=${
-                user.facebookInfo.fbToken}`, user)
-            }
-            return auth.setTokenCookie(req, res)
-          })(req, res, next)
-        })
-      })
-    })
-  } else {
+  //       CompanyProfile.findOne({_id: companyuser.companyId}).populate('planId').exec((err, company) => {
+  //         if (err) {
+  //           return res.status(501)
+  //             .json({status: 'failed', description: 'Internal Server Error'})
+  //         }
+  //         if (['plan_C', 'plan_D'].indexOf(company.planId.unique_ID) < 0) {
+  //           return res.status(401)
+  //             .json({
+  //               status: 'failed',
+  //               description: 'Given account information does not match any team account in our records'
+  //             })
+  //         }
+  //         passport.authenticate('email-local', function (err, user, info) {
+  //           var error = err || info
+  //           if (error) return res.status(501).json({status: 'failed', description: error.message, error: '' + JSON.stringify(error)})
+  //           if (!user) return res.json(404).json({message: 'User Not Found'})
+  //           req.user = user
+  //           if (user.facebookInfo) {
+  //             auth.fetchPages(`https://graph.facebook.com/v2.10/${
+  //               user.facebookInfo.fbId}/accounts?access_token=${
+  //               user.facebookInfo.fbToken}`, user)
+  //           }
+  //           return auth.setTokenCookie(req, res)
+  //         })(req, res, next)
+  //       })
+  //     })
+  //   })
+  //  } else {
     User.findOne({
       email: req.body.email.toLowerCase()
     }, (err, user) => {
@@ -103,12 +103,12 @@ router.post('/', function (req, res, next) {
             return res.status(501)
               .json({status: 'failed', description: 'Internal Server Error'})
           }
-          if (['plan_A', 'plan_B'].indexOf(company.planId.unique_ID) < 0) {
-            return res.status(401).json({
-              status: 'failed',
-              description: 'Given account information does not match any individual account in our records'
-            })
-          }
+          // if (['plan_A', 'plan_B'].indexOf(company.planId.unique_ID) < 0) {
+          //   return res.status(401).json({
+          //     status: 'failed',
+          //     description: 'Given account information does not match any individual account in our records'
+          //   })
+          // }
           passport.authenticate('email-local', function (err, user, info) {
             var error = err || info
             if (error) return res.status(501).json({status: 'failed', description: error.message, error: '' + JSON.stringify(error)})
@@ -124,7 +124,7 @@ router.post('/', function (req, res, next) {
         })
       })
     })
-  }
+  // }
 })
 
 // router.post('/', function (req, res, next) {
