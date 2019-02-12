5e345f50010e1f62067206aef5335ec8bb8a4187
anishachhatwani <i130315@nu.edu.pk>
2019-02-12 15:03:48 +0500

    file uploading

--- server/api/v1/files/utility.js
+++ server/api/v1/files/utility.js
@@ -1,13 +1,8 @@
-var whitelist = ['https://skiboengage.cloudkibo.com',
-  'https://skibochat.cloudkibo.com',
-  'http://localhost:3021',
-  'http://localhost:3022',
-  'https://kiboengage.cloudkibo.com',
-  'https://kibochat.cloudkibo.com']
+const config = require('./../../../config/environment/index')
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (config.whitelistedDomains.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
