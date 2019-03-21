2eb60310d58c591c960670cfc3a422e7a475ea14
Imran Shoukat <ishoukat@localhost.localdomain>
2019-03-21 14:40:47 +0500

    database modification

--- server/api/v1/tags/tags.model.js
+++ server/api/v1/tags/tags.model.js
@@ -5,7 +5,10 @@ const tagSchema = new Schema({
   tag: {type: String},
   userId: {type: Schema.ObjectId, ref: 'users'},
   companyId: {type: Schema.ObjectId, ref: 'companyprofile'},
-  dateCreated: {type: Date, default: Date.now}
+  dateCreated: {type: Date, default: Date.now},
+  pageId: String,
+  labelFbId: String,
+  defaultTag: {type: Boolean, default: false}
 })
 
 module.exports = mongoose.model('tags', tagSchema)
