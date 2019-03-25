ab58a79c6f5f2f49f1d91d43b1cea5b2c41fef21
Imran Shoukat <ishoukat@192.168.0.108>
2019-03-25 16:37:12 +0500

    script to normalize pages data

--- server/api/scripts/broadcastApi/index.js
+++ server/api/scripts/broadcastApi/index.js
@@ -5,5 +5,6 @@ const controller = require('./controller.js')
 router.get('/tagsDataForPageId', controller.normalizeTagsDataForPageId)
 router.get('/listsData', controller.normalizeListsData)
 router.get('/tagsData', controller.normalizeTagsData)
+router.get('/pagesData', controller.normalizePagesData)
 
 module.exports = router
