635840fc2c5192cc00c39b9e62b7ffc5fa9a378e
Imran Shoukat <ishoukat@192.168.0.108>
2019-03-26 15:12:32 +0500

    script to normalize tag subscribers

--- server/api/scripts/broadcastApi/index.js
+++ server/api/scripts/broadcastApi/index.js
@@ -6,5 +6,8 @@ router.get('/tagsDataForPageId', controller.normalizeTagsDataForPageId)
 router.get('/listsData', controller.normalizeListsData)
 router.get('/tagsData', controller.normalizeTagsData)
 router.get('/pagesData', controller.normalizePagesData)
+router.get('/tagSubscribers', controller.normalizeTagSubscribers)
+router.get('/tagSubscribersDefault', controller.normalizeTagSubscribersDefault)
+router.get('/tagSubscribersList', controller.normalizeTagSubscribersList)
 
 module.exports = router
