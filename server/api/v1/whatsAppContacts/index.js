6f60661db52d9d6ee598737dd3a88c094998c374
anishachhatwani <i130315@nu.edu.pk>
2019-04-08 14:04:25 +0500

    whatsapp twilio work

---
+++ server/api/v1/whatsAppContacts/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const validate = require('express-jsonschema').validate
+
+const validationSchema = require('./validationSchema')
+const controller = require('./whatsAppContacts.controller')
+const auth = require('./../../../auth/auth.service')
+
+router.post('/',
+  auth.isAuthenticated(),
+  validate({body: validationSchema.create}),
+  controller.create)
+
+router.post('/query',
+  auth.isAuthenticated(),
+  controller.query)
+
+router.post('/aggregate',
+  auth.isAuthenticated(),
+  controller.aggregate)
+
+router.put('/update',
+  validate({body: validationSchema.genericUpdatePayload}),
+  auth.isAuthenticated(),
+  controller.genericUpdate)
+
+module.exports = router
