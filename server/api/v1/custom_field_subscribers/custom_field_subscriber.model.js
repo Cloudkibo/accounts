9bd9bb5ebc4bc4f6a6c08417a6694365f628666f
Cloudkibo-Faizan <faizanrasheed4@gmail.com>
2019-07-17 17:34:42 +0500

    Delete Custom Field is not working properly

--- server/api/v1/custom_field_subscribers/custom_field_subscriber.model.js
+++ server/api/v1/custom_field_subscribers/custom_field_subscriber.model.js
@@ -2,9 +2,9 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
 const customFieldSubscibersSchema = new Schema({
-  customFieldId: {type: Schema.ObjectId, ref: 'custom_fields'},
-  subscriberId: {type: Schema.ObjectId, ref: 'subscribers'},
-  value: {type: String}
+  customFieldId: { type: Schema.ObjectId, ref: 'custom_fields' },
+  subscriberId: { type: Schema.ObjectId, ref: 'subscribers' },
+  value: { type: String }
 })
 
 module.exports = mongoose.model('custom_field_subscribers', customFieldSubscibersSchema)
