a8b0fb4cf21a08591e066c2a497bb7a0d6e935b8
anishachhatwani <i130315@nu.edu.pk>
2019-09-25 17:01:58 +0500

    unread count

--- server/api/scripts/apiCaller.js
+++ server/api/scripts/apiCaller.js
@@ -15,7 +15,6 @@ exports.callApi = (endpoint, method = 'get', body, token, type) => {
     }
   }
   let apiUrl = config.api_urls[type]
-  console.log('apiUrl', apiUrl)
   let options = {
     method: method.toUpperCase(),
     uri: `${apiUrl}/${endpoint}`,
