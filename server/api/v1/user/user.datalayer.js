a18d280542a607139e3e289485860b433b64132b
anishachhatwani <i130315@nu.edu.pk>
2020-03-19 16:39:52 +0500

    kiboDBLayer

--- server/api/v1/user/user.datalayer.js
+++ server/api/v1/user/user.datalayer.js
@@ -62,9 +62,6 @@ exports.findOneAndUpdateUsingQuery = (query, updated, options) => {
 }
 
 exports.genericUpdateUserObject = (query, updated, options) => {
-  console.log('query got', query)
-  console.log('options got', options)
-  console.log('updated got', updated)
   return UserModel.update(query, updated, options)
     .exec()
 }
