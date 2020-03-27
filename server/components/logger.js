be90aef1611667be45ce6a25dfc36088cec07f4c
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-03-27 16:04:24 +0500

    setup papertrail

--- server/components/logger.js
+++ server/components/logger.js
@@ -1,6 +1,22 @@
 const config = require('../config/environment/index')
+const winston = require('winston')
 
-exports.serverLog = function (label, data) {
+// eslint-disable-next-line no-unused-expressions
+require('winston-papertrail').Papertrail
+
+const logger = new winston.Logger({
+  transports: [
+    // new (winston.transports.Console)(),
+    new winston.transports.Papertrail({
+      host: 'logs3.papertrailapp.com',
+      port: 45576,
+      colorize: true,
+      attemptsBeforeDecay: 1
+    })
+  ]
+})
+
+exports.serverLog = function (label, data, type = 'info', show = false) {
   const namespace = `accounts:${label}`
   const debug = require('debug')(namespace)
 
@@ -8,8 +24,13 @@ exports.serverLog = function (label, data) {
     debug(data)
     // todo use log levels like info, warn, error and debug
     // logger.info(`${namespace} - ${data}`)
+  } else {
+    if (show && config.env === 'production' && ['info', 'error'].indexOf(type) > -1) {
+      logger.log(type, `${namespace} - ${data}`)
+    } else {
+      console.log(data)
+    }
   }
-  console.log(data)
 }
 
 exports.clientLog = function (label, data) {
