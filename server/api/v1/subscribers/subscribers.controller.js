5b985a5a322cd82cfd372fc5efc9f10cb1929761
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-03-31 16:58:46 +0500

    subscriber check

--- server/api/v1/subscribers/subscribers.controller.js
+++ server/api/v1/subscribers/subscribers.controller.js
@@ -22,9 +22,17 @@ exports.index = function (req, res) {
 
 exports.create = function (req, res) {
   logger.serverLog(TAG, `Hit the create subscriber controller index ${JSON.stringify(req.body)}`, 'info', true)
-  subscribersDataLayer.createSubscriberObject(req.body)
-    .then(result => {
-      sendSuccessResponse(res, 200, result)
+  subscribersDataLayer.findSubscriberObjects({senderId: req.body.senderId, pageId: req.body.pageId, companyId: req.body.companyId})
+    .then(subscribers => {
+      if (subscribers.length === 0) {
+        subscribersDataLayer.createSubscriberObject(req.body)
+          .then(result => {
+            sendSuccessResponse(res, 200, result)
+          })
+          .catch(err => {
+            sendErrorResponse(res, 500, err)
+          })
+      }
     })
     .catch(err => {
       sendErrorResponse(res, 500, err)
