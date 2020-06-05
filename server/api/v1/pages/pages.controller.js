17cd500dca6ccd16a04d2b32bcf4330c2136734c
sania <sania@cloudkibo.com>
2020-06-05 16:05:39 +0500

    Refresh pages

--- server/api/v1/pages/pages.controller.js
+++ server/api/v1/pages/pages.controller.js
@@ -451,7 +451,7 @@ function fetchPages (url, user, res) {
       return res.status(200).json({status: 'success', payload: []})
     }
     if (cursor && cursor.next) {
-      fetchPages(cursor.next, user, rs)
+      fetchPages(cursor.next, user, res)
     } else {
       logger.serverLog(TAG, 'Undefined Cursor from graph API')
     }
