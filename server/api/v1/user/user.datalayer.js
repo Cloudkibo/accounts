4e054c42321d8fb8a15d00631bf115b7134eda96
arveenkumar55 <arveen@cloudkibo.com>
2020-07-01 10:39:52 +0500

    fix_email_bug

--- server/api/v1/user/user.datalayer.js
+++ server/api/v1/user/user.datalayer.js
@@ -35,12 +35,12 @@ exports.createUserObject = (payload) => {
 }
 
 exports.findOneUserByEmail = (body) => {
-  return UserModel.findOne({email: body.email})
+  return UserModel.findOne({email: {$regex: `^${body.email}$`, $options: 'i'}})
     .exec()
 }
 
 exports.findOneUserByDomain = (body) => {
-  return UserModel.findOne({domain: body.domain})
+  return UserModel.findOne({domain: {$regex: `^${body.domain}$`, $options: 'i'}})
     .exec()
 }
 
