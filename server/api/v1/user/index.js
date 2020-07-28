092e5a5d7eb949ef1ac560782a55feb07376ce82
Sojharo <sojharo@live.com>
2020-07-28 21:41:56 +0500

    put disable accounts CloudKibo/KiboPush#9234

--- server/api/v1/user/index.js
+++ server/api/v1/user/index.js
@@ -18,7 +18,7 @@ router.post('/authenticatePassword',
   controller.authenticatePassword)
 
 router.post('/markAccountAsDisabled',
-//  auth.isAuthorizedSuperUser(),
+  auth.isAuthorizedSuperUser(),
   controller.markAccountAsDisabled)
 
 router.post('/updateMode',
