79ccf2ccfbb16aa9707473b55d769d2e76c8af4f
sania <sania@cloudkibo.com>
2020-08-06 00:59:04 +0500

    Typo

--- server/api/v1/companyPreferences/companyPreferences.datalayer.js
+++ server/api/v1/companyPreferences/companyPreferences.datalayer.js
@@ -3,7 +3,7 @@ This file will contain the functions for data layer.
 By separating it from controller, we are separating the concerns.
 Thus we can use it from other non express callers like cron etc
 */
-const CompanyPreferencesModel = require('./companypreferences.model')
+const CompanyPreferencesModel = require('./companyPreferences.model')
 
 
 exports.findAllCompanyPreferencesUsingQuery = (queryObject) => {
