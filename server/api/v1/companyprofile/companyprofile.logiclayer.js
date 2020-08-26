e4ad6e23459faea7bee2aac1894f37df12bc494f
anishachhatwani <i130315@nu.edu.pk>
2020-08-26 11:51:32 +0500

    restring whatsapp number

--- server/api/v1/companyprofile/companyprofile.logiclayer.js
+++ server/api/v1/companyprofile/companyprofile.logiclayer.js
@@ -107,8 +107,7 @@ exports.validateAndConvert = (body) => {
   body.forEach((obj, index) => {
     if (obj.$match && obj.$match._id && typeof obj.$match._id === 'string') {
       newBody[index].$match._id = mongoose.Types.ObjectId(newBody[index].$match._id)
-    }
-    if (obj.$match && obj.$match._id && obj.$match._id.$ne) {
+    } else if (obj.$match && obj.$match._id && obj.$match._id.$ne) {
       newBody[index].$match._id.$ne = mongoose.Types.ObjectId(newBody[index].$match._id.$ne)
     }
   })
