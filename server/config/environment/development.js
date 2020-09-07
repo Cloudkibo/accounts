e806d8740d01c31201962fee32e9681c6d2e3f18
sania <sania@cloudkibo.com>
2020-09-07 22:01:26 +0500

    CRUD for companyAddOns

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -14,7 +14,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/accounts-prod'
+    uri: 'mongodb://localhost/accounts-dev'
   },
   seedDB: false,
 
